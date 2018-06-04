63c7a7d04fce86636d8f6c99f3cf47b0ccab9a33
André Michel Andy Pozos <andreandyp@outlook.com>
2018-06-03 19:07:18 -0500

    Probando transmision doble

--- public/js/app.js
+++ public/js/app.js
@@ -1,13 +1,27 @@
 var webrtc = new SimpleWebRTC({
 	localVideoEl: "conferencista",
-	remoteVideoEl: "invitado",
+	remoteVideoEl: "",
 	autoRequestMedia: true
 });
 
 webrtc.on("readyToCall", () => webrtc.joinRoom("prueba"));
+webrtc.on("videoAdded", function (video, peer) {
+	console.log("video added", peer);
+	var invitado = document.getElementById("invitado");
+	var d = document.createElement("div");
+	d.className = "videoContainer";
+	d.id = "container_" + webrtc.getDomId(peer);
+	d.appendChild(video);
+	video.onclick = function () {
+		video.style.width = video.videoWidth + "px";
+		video.style.height = video.videoHeight + "px";
+	};
+	invitado.appendChild(d);
+});
 
+
+
+/*
 const socket = io();
-
 document.querySelector("#prueba").addEventListener("click", () => socket.emit("prueba", "Hey qué pedo carnal"), true);
-
-socket.on("respuesta", mensaje => alert("Respuesta: "+mensaje));
+socket.on("respuesta", mensaje => alert("Respuesta: "+mensaje));*/
